0398e91dfd64493b8a5edff30f41f9360b287125
amar <amarbaliyan147@gmail.com>
2025-08-10 17:00:54 +0530

    updates

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -10,12 +11,15 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
-app.set('trust proxy',true);
-app.use(cookieSession({
-  signed:false,
-  secure:process.env.NODE_ENV!=='test'
-}))
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
@@ -27,4 +31,4 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-export {app};
+export { app };
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -3,20 +3,22 @@ import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
 import { Password } from '../services/password';
-import { User } from '../model/User';
-import { validateRequest } from '@ecam/common';
-import { BadRequestError } from '@ecam/common';
+import { User } from '../models/user';
+import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post(
   '/api/users/signin',
   [
-    body('email').isEmail().withMessage('Email must be valid'),
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
     body('password')
       .trim()
       .notEmpty()
-      .withMessage('You must supply a password'),
+      .withMessage('You must supply a password')
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -39,14 +41,14 @@ router.post(
     const userJwt = jwt.sign(
       {
         id: existingUser.id,
-        email: existingUser.email,
+        email: existingUser.email
       },
       process.env.JWT_KEY!
     );
 
     // Store it on session object
     req.session = {
-      jwt: userJwt,
+      jwt: userJwt
     };
 
     res.status(200).send(existingUser);
