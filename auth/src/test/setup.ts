022ff2c1376be3b9e2a56da76fd97081fc3d138d
Amar Singh <amarsingh147@ibm.com>
2026-04-16 22:21:43 +0530

    next batch

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -2,6 +2,7 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../app";
+import { beforeAll,beforeEach,afterAll } from '@jest/globals';
 
 declare global {
   var signin: () => Promise<string[]>;
